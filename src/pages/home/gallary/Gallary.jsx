804666f6aa5e245862b2f4021ad9d41e3e0136f1
shaykot43500 <hossainshaykot@gmail.com>
2023-05-20 12:33:38 +0600

    added new change gallary

--- src/pages/home/gallary/Gallary.jsx
+++ src/pages/home/gallary/Gallary.jsx
@@ -1,50 +1,37 @@
 import React from 'react';
-import abacus1 from "../../../assets/gallary/abacus1.jpg"
-import abacus2 from "../../../assets/gallary/abacus2.jpg"
+import math1 from "../../../assets/gallary/math1.jpg"
+import math2 from "../../../assets/gallary/math2.png"
+import science1 from "../../../assets/gallary/science1.jpg"
+import science2 from "../../../assets/gallary/science2.png"
+import engineering1 from "../../../assets/gallary/engineering1.png"
+import engineering2 from "../../../assets/gallary/engineering2.jpg"
 import abacus3 from "../../../assets/gallary/abacus3.jpg"
-import bike from "../../../assets/gallary/bike.jpg"
-import blacksmith from "../../../assets/gallary/blacksmith.jpg"
 
-import calculator from "../../../assets/gallary/calculator.jpg"
-import carnes from "../../../assets/gallary/cranes.jpg"
-import education from "../../../assets/gallary/education.jpg"
-import hook from "../../../assets/gallary/hook.jpg"
-import musician from "../../../assets/gallary/musician.jpg"
-import musician2 from "../../../assets/gallary/musician2.jpg"
 
-import pocket from "../../../assets/gallary/pocket.jpg"
-
- import welding from "../../../assets/gallary/welding.jpg"
 import { PhotoAlbum } from 'react-photo-album';
 // import PhotoAlbum from './PhotoAlbum';
 
 const Gallary = () => {
     const photos = [
-        {src:abacus1, width:800, height:600},
-        {src:abacus2, width:1600, height:900},
-        {src:abacus3, width:1200, height:900},
-        {src:bike, width:1600, height:900},
-        {src:blacksmith, width:1400, height:800},
+        {src:engineering1, width:800, height:800},
+        {src:abacus3, width:1200, height:600},
+        {src:science1, width:800, height:800},
+        {src:math1, width:1200, height:600},
+        {src:engineering2, width:800, height:800},
+        {src:science2, width:1200, height:600},
+        {src:math2, width:800, height:800},
         
-        {src:calculator, width:1900, height:1200},
-        {src:carnes, width:1600, height:900},
-        {src:education, width:600, height:900},
-        {src:hook, width:1600, height:900},
-        {src:musician, width:800, height:900},
-        {src:musician2, width:1600, height:900},
         
-        {src:pocket, width:600, height:900},
-        {src:welding, width:1600, height:900},
     ]
    
     return (
-        <div>
+        <div className='container mx-auto'>
             <div data-aos="flip-down" className=' p-16'>
             <p className='text-center text-3xl font-bold text-black p-2'>Science & Engineering Kits Gallery</p>
                 <p className='mx-auto text-center text-black lg:w-2/4'>The science and engineering kit gallery offers a diverse range of educational resources. From model kits of famous experiments to DIY circuit building kits, these hands-on tools engage learners in understanding electrical engineering, robotics, and more. </p>
             </div>
             <div className='container mx-auto'>
-                       {
+            {
                 <PhotoAlbum layout="rows"
                     photos={photos}
                 ></PhotoAlbum>
